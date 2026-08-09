2ff6c2a58028389d1e17b45e4a953a08312e6a2e
NMP039 <minhphat3920062006@gmail.com>
2026-08-09 15:55:57 +0700

    update tc

--- e2e/tests/aaaa.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:5173/login');
-  await page.locator('div').nth(2).click();
-});
